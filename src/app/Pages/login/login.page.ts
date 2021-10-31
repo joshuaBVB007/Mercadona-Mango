619a8498cdea9e2d86c194bd68e42e7a1cff70e6
Jonathan Calderon <jaksvafa7@gmail.com>
2021-10-31 14:15:10 +0100

    errores

--- src/app/Pages/login/login.page.ts
+++ src/app/Pages/login/login.page.ts
@@ -2,9 +2,6 @@ import { SearchProductsService } from 'src/app/Services/search-products.service'
 import { Component, NgModule, OnInit } from '@angular/core';
 import { BBDDService } from 'src/app/Services/bbdd.service';
 import { Router, NavigationExtras, ActivatedRoute } from '@angular/router';
-import { MatomoTracker } from '@ngx-matomo/tracker';
-import { indices } from 'src/app/app.module';
-
 
 @Component({
   selector: 'app-login',
@@ -23,18 +20,13 @@ export class LoginPage implements OnInit {
   img_mango="../../../assets/vestidos/mango_logo2.jpg";
   button_arriba_style="";
   button_abajo_style="";
-  constructor(private readonly tracker: MatomoTracker,private routeInfo:ActivatedRoute,private search:SearchProductsService,private bbddService: BBDDService,private router: Router) {
+  constructor(private routeInfo:ActivatedRoute,private search:SearchProductsService,private bbddService: BBDDService,private router: Router) {
     this.PageName = this.bbddService.PageName(1);
   }
 
   ngOnInit() {
       
-      //informa en el dashboard cuantos han entrado en mi web
-      this.tracker.setUserId("manolita");
-      //informa que se ha visitado  la pagina
-      this.tracker.trackPageView("Han entrado en login");
-      console.log(this.tracker.getMatomoUrl());
-
+  
 
       this.asignarStylo=this.routeInfo.snapshot.params['stylo'];
       console.log(this.asignarStylo);
@@ -61,7 +53,6 @@ export class LoginPage implements OnInit {
   }
   OpenComprar() {
     //param1=categoria(video,comida,etc) param2=accion(cliente clicó en ir a productos)según manual de matomo.
-    this.tracker.trackEvent("Botón comprar","cliente clicó en ir a productos");
 
     const navigationExtras: NavigationExtras = { state: { prova: "prova" } };
     console.log("Redirecting to home page");
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,32 +2,22 @@ import { PipesModule } from './pipes/pipes.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
-import { NgxMatomoTrackerModule } from '@ngx-matomo/tracker';
 import { HeaderPageModule } from './common/header/header.module';
 import { ProductsPageModule } from './Pages/products/products.module';
 import { RecogidaPageModule } from './Pages/recogida/recogida.module';
 
-export var indices={
-  indice:"1",
-};
 
 @NgModule({
-  declarations: [AppComponent,  ],
+  declarations: [AppComponent],
   entryComponents: [],
   imports: [
     BrowserModule,
     IonicModule.forRoot(),
-    NgxMatomoTrackerModule.forRoot({
-      siteId: indices.indice, // your Matomo's site ID (find it in your Matomo's settings)
-      trackerUrl: 'https://jona.matomo.cloud', // your matomo server root url
-    }),
     AppRoutingModule,
     PipesModule,
     HttpClientModule,
